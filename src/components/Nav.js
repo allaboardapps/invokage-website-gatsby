3bbcb749e4703c8ac6fa8f8f9779b5e9f10e6c5c
Randy Burgess <wrburgess@gmail.com>
2018-06-09 12:44:58 -0500

    edits for site

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,30 +3,30 @@ import Scrollspy from 'react-scrollspy'
 import Scroll from './Scroll'
 
 const Nav = (props) => (
-    <nav id="nav" className={props.sticky ? 'alt' : ''}>
-        <Scrollspy items={ ['intro', 'first', 'second', 'cta'] } currentClassName="is-active" offset={-300}>
-            <li>
-                <Scroll type="id" element="intro">
-                    <a href="#">Introduction</a>
-                </Scroll>
-            </li>
-            <li>
-                <Scroll type="id" element="first">
-                    <a href="#">First Section</a>
-                </Scroll>
-            </li>
-            <li>
-                <Scroll type="id" element="second">
-                    <a href="#">Second Section</a>
-                </Scroll>
-            </li>
-            <li>
-                <Scroll type="id" element="cta">
-                    <a href="#">Get Started</a>
-                </Scroll>
-            </li>
-        </Scrollspy>
-    </nav>
+  <nav id="nav" className={props.sticky ? 'alt' : ''}>
+    <Scrollspy items={ ['intro', 'first', 'second', 'cta'] } currentClassName="is-active" offset={-300}>
+      <li>
+        <Scroll type="id" element="intro">
+          <a href="#introduction">Introduction</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="products">
+          <a href="#products">Products</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="podcasts">
+          <a href="#podcasts">Podcasts</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="footer">
+          <a href="#footer">Contact Us</a>
+        </Scroll>
+      </li>
+    </Scrollspy>
+  </nav>
 )
 
 export default Nav
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,10 @@ import Waypoint from 'react-waypoint'
 
 import Header from '../components/Header'
 import Nav from '../components/Nav'
-import pic01 from '../assets/images/pic01.jpg'
+import whoWeAre from '../assets/images/who_we_are_image.png'
+import ctoThinkLogo from '../assets/images/cto_think_logo.png'
+import thisOldAppLogo from '../assets/images/this_old_app_logo.jpg'
+import '../assets/scss/index.scss';
 
 class Index extends React.Component {
   constructor(props) {
@@ -45,99 +48,59 @@ class Index extends React.Component {
             <div className="spotlight">
               <div className="content">
                 <header className="major">
-                  <h2>Who We Are( )</h2>
+                  <h2>Who We Are</h2>
                 </header>
-                <p>Sed lorem ipsum dolor sit amet nullam consequat feugiat consequat magna
-                adipiscing magna etiam amet veroeros. Lorem ipsum dolor tempus sit cursus.
-                Tempus nisl et nullam lorem ipsum dolor sit amet aliquam.</p>
+                <p>Don VanDemark and Randy Burgess are the founders of Invokage. Friends for over a decade, they finally decided to put their heads together and build things.</p>
                 <ul className="actions">
                   <li><Link to="/generic" className="button">Learn More</Link></li>
                 </ul>
               </div>
-              <span className="image"><img src={pic01} alt="" /></span>
+              <span className="image"><img src={whoWeAre} alt="Don VanDemark and Randy Burgess portraits" /></span>
             </div>
           </section>
 
-          <section id="first" className="main special">
+          <section id="products" className="main special">
             <header className="major">
               <h2>Our Products</h2>
             </header>
             <ul className="features">
               <li>
-                <span className="icon major style1 fa-code"></span>
-                <h3>Ipsum consequat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <span className="icon major style3 fa-microphone"></span>
+                <h3>Ref Check</h3>
+                <p>A voice interface (think Alexa, Siri, and Google Assistant) for training football referees.</p>
               </li>
               <li>
-                <span className="icon major style3 fa-copy"></span>
-                <h3>Amed sed feugiat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <span className="icon major style1 fa-comments"></span>
+                <h3>Chasms</h3>
+                <p>A system to allow "folks in the field" to communicate via low-cost, low-bandwidth means (think SMS) to a single channel with mutliple dispatchers.</p>
+              </li>
+            </ul>
+          </section>
+
+          <section id="podcasts" className="main special">
+            <header className="major">
+              <h2>Our Podcasts</h2>
+            </header>
+            <ul className="features">
+              <li>
+                <span className="icon">
+                  <a href="https://www.ctothink.com">
+                    <img src={ctoThinkLogo} style={{width: 200, height: 200}} alt="CTO Think Podcast Logo" />
+                  </a>
+                </span>
+                <p>A pragmatic podcast about leadership, product dev, and tech decisions between two recovering Chief Technology Officers.</p>
               </li>
               <li>
-                <span className="icon major style5 fa-diamond"></span>
-                <h3>Dolor nullam</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <span className="icon">
+                  <a href="https://thisoldapp.online">
+                    <img src={thisOldAppLogo} style={{ width: 200, height: 200 }} alt="This Old App Podcast Logo" />
+                  </a>
+                </span>
+                <p>A podcast about learning, coding, smashing stuff together, breaking things apart, startups, failing, winning, and any other buzzwords we can think of.</p>
               </li>
             </ul>
-            <footer className="major">
-              <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
-              </ul>
-            </footer>
           </section>
-
-          <section id="second" className="main special">
-            <header className="major">
-              <h2>Ipsum consequat</h2>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
-            </header>
-            <ul className="statistics">
-              <li className="style1">
-                <span className="icon fa-code-fork"></span>
-                <strong>5,120</strong> Etiam
-              </li>
-              <li className="style2">
-                <span className="icon fa-folder-open-o"></span>
-                <strong>8,192</strong> Magna
-              </li>
-              <li className="style3">
-                <span className="icon fa-signal"></span>
-                <strong>2,048</strong> Tempus
-              </li>
-              <li className="style4">
-                <span className="icon fa-laptop"></span>
-                <strong>4,096</strong> Aliquam
-              </li>
-              <li className="style5">
-                <span className="icon fa-diamond"></span>
-                <strong>1,024</strong> Nullam
-              </li>
-            </ul>
-            <p className="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.</p>
-            <footer className="major">
-              <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
-              </ul>
-            </footer>
-          </section>
-
-          <section id="cta" className="main special">
-            <header className="major">
-              <h2>Congue imperdiet</h2>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
-            </header>
-            <footer className="major">
-              <ul className="actions">
-                <li><Link to="/generic" className="button special">Get Started</Link></li>
-                <li><Link to="/generic" className="button">Learn More</Link></li>
-              </ul>
-            </footer>
-          </section>
-
         </div>
-
       </div>
     )
   }
